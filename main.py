2f690e19cc7548dd7383ecb9ef806032aa089aa5
ebucheli <edoardobucheli@gmail.com>
2019-11-28 11:40:22 -0600

    general updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ from data_loader import load_data, make_partition
 parser = argparse.ArgumentParser()
 parser.add_argument('--training_data', dest = 'train_path')
 parser.add_argument('--testing_data', dest = 'test_path')
-parser.add_argument('--random_initialization', action= 'store_true')
+parser.add_argument('--rand_init', action= 'store_true')
 parser.add_argument('--outfile', dest = 'outfile')
 
 args = parser.parse_args()
@@ -28,7 +29,7 @@ if __name__ == '__main__':
 
     train_path = args.train_path
     test_path = args.test_path
-    rand_init = args.random_initialization
+    rand_init = args.rand_init
     outfile = args.outfile
 
     jvm.start()
@@ -37,10 +38,26 @@ if __name__ == '__main__':
     N_train = data_train.num_instances
     #data_normal, N = make_partition(data,attributes)
 
+    remove = Filter(classname='weka.filters.unsupervised.attribute.Remove',
+                    options = ['-R','last'])
+    remove.inputformat(data_train)
+    data_train = remove.filter(data_train)
+
+    m_train = data_train.num_attributes
+
     print('\n\n############ Training Decision Trees ############\n\n')
 
     clfs,dt_y_hat = train_trees(data_train,attributes)
+
     w2_init,w1_init,b1_init = get_initial_weights(data_train,clfs,attributes,dt_y_hat)
+
+    if rand_init == True:
+
+        w1_init = []
+        for i in range(m_train):
+            len_values = len(data_train.attribute(i).values)
+            w1_init.append(np.random.randn(len_values))
+
 
     print('\n\nDone!')
 
@@ -61,7 +78,10 @@ if __name__ == '__main__':
 
     res,my_score = test(data_test,N_test,attributes_test,clfs,w1,b1,w2_init)
 
-
+    #with open(outfile,'w') as f:
+        #f.write('############ DTAE Report ############\n\n')
+        #f.write(': {}\n\n'.format(res))
+        #f.write(''str(my_score))
 
     print('\n\n############ Final AUC Score on Testing Data ############\n')
 
@@ -70,5 +90,13 @@ if __name__ == '__main__':
     res2,my_score2 = test(data_test,N_test,attributes_test,clfs,w1_init,b1_init,w2_init)
 
     print('Score using initial values: {}\n\n'.format(my_score2))
+
+    dict_res = {'AUC_trained':my_score,
+                'scores_trained':res.tolist(),
+                'AUC_initial':my_score2,
+                'scores_initial':res2.tolist()}
+
+    with open(outfile, 'w') as fp:
+        json.dump(dict_res, fp)
 
     jvm.stop()
